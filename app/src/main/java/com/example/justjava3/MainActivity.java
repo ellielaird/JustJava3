2787ebc53afd2201dff1ecb4aef98e19aedf8760
Ellie Laird <ellielaird@icloud.com>
2022-09-20 13:16:42 -0500

    layout updated

--- app/src/main/java/com/example/justjava3/MainActivity.java
+++ app/src/main/java/com/example/justjava3/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
