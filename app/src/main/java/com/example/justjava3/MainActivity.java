d02e42b3ff4ec364377cc6ed9743bf4d1a46ab3d
Ellie Laird <ellielaird@icloud.com>
2022-09-27 11:48:39 -0500

    added name box, updated price, able to send emails now

--- app/src/main/java/com/example/justjava3/MainActivity.java
+++ app/src/main/java/com/example/justjava3/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.justjava3;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     int quantity;
     String message;
+    int price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,19 +50,29 @@ public class MainActivity extends AppCompatActivity {
     private void order(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-        message = "Add whipped cream? " + hasWhip() + "\nAdd chocolate? " + hasChocolate() + "\nQuantity: " + quantity + "\nTotal: $" + (quantity * 5) + "\nThank you!";
+        EditText name = (EditText) findViewById(R.id.insert_name);
+        message = "Name: " + name.getText() + "\nAdd whipped cream? " + hasWhip() + "\nAdd chocolate? " + hasChocolate() + "\nQuantity: " + quantity + "\nTotal: $" + getPrice()  + "\nThank you!";
         displayMessage(message);
+        composeEmail();
     }
 
 
     public void increment(View view){
         quantity++;
+        if (quantity > 100){
+            quantity= 100;
+            display(quantity);
+        }
         display(quantity);
 
     }
 
     public void decrement(View view){
         quantity--;
+        if (quantity < 0) {
+            quantity = 0;
+            display(quantity);
+        }
         display(quantity);
     }
 
@@ -76,8 +89,10 @@ public class MainActivity extends AppCompatActivity {
         boolean hasWhip = false;
 
         CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.whippedcream_checkbox);
-        if (whippedCreamCheckbox.isChecked())
+        if (whippedCreamCheckbox.isChecked()) {
             hasWhip = true;
+        }
+
 
           return hasWhip;
     }
@@ -86,11 +101,34 @@ public class MainActivity extends AppCompatActivity {
     {
         boolean hasChocolate = false;
         CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.chocolate_checkbox);
-        if (chocolateCheckbox.isChecked())
+        if (chocolateCheckbox.isChecked()){
             hasChocolate = true;
+        }
 
         return hasChocolate;
     }
+
+    public int getPrice()
+    {
+        price = quantity * 5;
+        if (hasWhip() == true && hasChocolate()==true)
+            price = price +3;
+
+        else if (hasWhip()==true)
+            price = price + quantity;
+
+        else if (hasChocolate()==true)
+            price= price + quantity * 2;
+
+        return price;
+    }
+
+    public void composeEmail() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 }
-
-
